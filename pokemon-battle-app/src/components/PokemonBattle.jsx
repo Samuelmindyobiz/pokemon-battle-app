55d00b5a5b7b58457c1e3095e30aa2409e006d27
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-05-17 10:38:03 +0200

    battle started

--- pokemon-battle-app/src/components/PokemonBattle.jsx
+++ pokemon-battle-app/src/components/PokemonBattle.jsx
@@ -61,73 +61,118 @@ const PokemonGame = () => {
     }, []);
 
     if (loading) return <p>Loading...</p>
+    const startBattle = () => {
+        const playerAttack = Math.floor(Math.random() * 100) + 1;
+        const computerAttack = Math.floor(Math.random() * 100) + 1;
 
+        if (playerAttack > computerAttack) {
+            setBattleResult("You win!");
+        } else if (playerAttack < computerAttack) {
+            setBattleResult("You lose!");
+        } else {
+            setBattleResult("It's a tie!");
+        }
+    }
+
+
+    const defend = () => {
+        const playerDefend = Math.floor(Math.random() * 100) + 1;
+        const computerDefend = Math.floor(Math.random() * 100) + 1;
+
+        if (playerDefend > computerDefend) {
+            setBattleResult("You win!");
+        } else if (playerDefend < computerDefend) {
+            setBattleResult("You lose!");
+        } else {
+            setBattleResult("It's a tie!");
+        }
+    }
 
     return (
+      <div>
+        <h1>Pokemon Battle Game</h1>
         <div>
-            <h1>Pokemon Battle Game</h1>
-            <div>
-                <h2>Select Your Pokemon:</h2>
-                <div className="random-card">
-                    <ul>
-                        <li>
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardMedia
-                                    sx={{ height: 140, width: 140, margin: 10 }}
-                                    image={`https://img.pokemondb.net/artwork/large/${pokemonList.name}.jpg`}
-                                    title="green iguana"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {pokemonList.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {pokemonList.types &&
-                                            pokemonList.types.map((type, index) => (
-                                                <li key={index}>{type.type.name}</li>
-                                            ))}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions className="action-btn">
-                                    <Button color="error" size="small">Attack</Button>
-                                    <Button color="success" size="small">Defend</Button>
-                                </CardActions>
-                            </Card>
+          <h2>Select Your Pokemon:</h2>
+          <div className="random-card">
+            <ul>
+              <li>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    sx={{ height: 140, width: 140, margin: 10 }}
+                    image={`https://img.pokemondb.net/artwork/large/${pokemonList.name}.jpg`}
+                    title="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {pokemonList.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {pokemonList.types &&
+                        pokemonList.types.map((type, index) => (
+                          <li key={index}>{type.type.name}</li>
+                        ))}
+                    </Typography>
+                  </CardContent>
+                  <CardActions className="action-btn">
+                    <Button color="error" size="small" onClick={startBattle}>
+                      Attack
+                    </Button>
+                    <Button color="success" size="small" onClick={defend}>
+                      Defend
+                    </Button>
+                  </CardActions>
+                </Card>
+              </li>
+            </ul>
 
-                        </li>
-                    </ul>
-
-                    <ul >
-                        <li >
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardMedia
-                                    sx={{ height: 140, width: 140, margin: 10 }}
-                                    image={`https://img.pokemondb.net/artwork/large/${computerPokemon.name}.jpg`}
-                                    title="green iguana"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {computerPokemon.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {computerPokemon.types &&
-                                            computerPokemon.types.map((type, index) => (
-                                                <li key={index}>{type.type.name}</li>
-                                            ))}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions className="action-btn">
-                                    <Button color="error" size="small">Attack</Button>
-                                    <Button color="success" size="small">Defend</Button>
-                                </CardActions>
-                            </Card>
-
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            <ul>
+              <li>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    sx={{ height: 140, width: 140, margin: 10 }}
+                    image={`https://img.pokemondb.net/artwork/large/${computerPokemon.name}.jpg`}
+                    title="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {computerPokemon.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {computerPokemon.types &&
+                        computerPokemon.types.map((type, index) => (
+                          <li key={index}>{type.type.name}</li>
+                        ))}
+                    </Typography>
+                  </CardContent>
+                  <CardActions className="action-btn">
+                    <Button color="error" size="small" onClick={startBattle}>
+                      Attack
+                    </Button>
+                    <Button color="success" size="small" onClick={defend}>
+                      Defend
+                    </Button>
+                  </CardActions>
+                </Card>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+        <div>
+          <h2>Battle Result:</h2>
+          <p>{battleResult}</p>
+
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={fetchRandomPokemon}
+            >
+                Play Again
+            </Button>
+
+      </div>
+  
+    </div>);
 };
+
 
 export default PokemonGame;
